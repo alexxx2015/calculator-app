878dc9f7e35a3525f50f6484e6c66ee59b598930
fromm <fromm@cs.tum.edu>
2017-03-29 18:34:41 +0200

    GAE update

--- src/main/java/edu/tum/cal/client/wsdl/package-info.java
+++ src/main/java/edu/tum/cal/client/wsdl/package-info.java
@@ -2,7 +2,7 @@
 // Diese Datei wurde mit der JavaTM Architecture for XML Binding(JAXB) Reference Implementation, v2.2.11 generiert 
 // Siehe <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
 // Änderungen an dieser Datei gehen bei einer Neukompilierung des Quellschemas verloren. 
-// Generiert: 2017.03.29 um 06:30:15 PM CEST 
+// Generiert: 2017.03.29 um 06:34:40 PM CEST 
 //
 
 @javax.xml.bind.annotation.XmlSchema(namespace = "http://mul.cal.tum.edu", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
